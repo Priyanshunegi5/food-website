d1fa19809dc8f92a7ffecb558405848696a88eac
Raman Verma <devramanverma@gmail.com>
2023-11-15 13:27:47 +0530

    Added: header and footer in root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import './globals.css'
+
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -46,7 +49,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${inter.className}`}>{children}</body>
+            <body className={`${inter.className}`}>
+                <Header />
+                {children}
+                <Footer />
+            </body>
         </html>
     )
 }
