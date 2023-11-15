e580c92f2e05ea23d40fe10b04414337f1787419
Raman Verma <devramanverma@gmail.com>
2023-11-15 13:28:18 +0530

    Added: footer boilerplate

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer(props) {
+    return (
+        <div>
+            Footer
+            {props.children}
+        </div>
+    )
+}
+
+export default Footer
