9d9d998d3f1014bf617f4509c3c2f4ef4aee7492
Raman Verma <devramanverma@gmail.com>
2023-11-19 01:41:09 +0530

    Updated: Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,48 @@
-import Link from 'next/link'
-import { Button, ButtonGroup } from '@nextui-org/react'
+import { headers } from 'next/headers'
+import { setting } from '@/data/setting'
+import { web } from '@/config/router/web'
+import useRouter from '@/hooks/useRouter'
+import Hero from '@/app/_components/hero'
+import WhyUs from '@/app/_components/why-us'
+import { layouts } from '@/data/layouts/layout'
+import Reviews from '@/app/_components/reviews'
+import Layout from '@/components/layouts/layout'
+import BlogInfo from '@/app/_components/blog-info'
+import MenuShort from '@/app/_components/menu-short'
+import MenuLarge from '@/app/_components/menu-large'
 
-export default function () {
+export const metadata = {}
+
+export default function Home() {
+    const url = headers().get('x-pathname') || '/'
+    const route = useRouter(web).getRouteByPath(url)
+
+    if (route !== null) {
+        metadata.description = setting.description
+        metadata.title = `${setting?.shortName} - ${route?.name}`
+    }
+
     return (
-        <>
-            <h1>Home Page</h1>
-            <br />
-            <Link href="/contact">
-                <Button color="primary">Contact Us</Button>
-            </Link>
-        </>
+        <Layout type={layouts.maximmal}>
+            {/* Highlights */}
+            <>
+                <Hero />
+                <WhyUs />
+            </>
+
+            {/* Products */}
+            <>
+                <MenuShort />
+                <MenuLarge />
+            </>
+
+            {/* References */}
+            <>
+                {/* From People */}
+                <Reviews />
+                {/* From Website */}
+                <BlogInfo />
+            </>
+        </Layout>
     )
 }
