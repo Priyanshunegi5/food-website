23304a07199c1bec8986e4513c83722918c25595
Raman Verma <devramanverma@gmail.com>
2023-11-18 23:55:27 +0530

    Added: ArrowRightIcon component

---
+++ src/components/icons/arrow-right-icon.js
@@ -0,0 +1,26 @@
+export default function ArrowRightIcon({
+    fill = 'currentColor',
+    filled,
+    size,
+    height,
+    width,
+    label,
+    ...props
+}) {
+    return (
+        <svg
+            width={size || width || 24}
+            height={size || height || 24}
+            viewBox="0 0 24 24"
+            fill={filled ? fill : 'none'}
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+            ></path>
+        </svg>
+    )
+}
