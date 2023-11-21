9f17003c49cb9bc5dcd63f26063cbffb574158e3
Raman Verma <devramanverma@gmail.com>
2023-11-21 23:40:12 +0530

    Refactored: Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,23 @@
 'use client'
 
+import React from 'react'
 import NextLink from 'next/link'
-import { setting } from '@/data/setting'
 import { web } from '@/config/router/web'
 import useRouter from '@/hooks/useRouter'
-import Logo from '@/components/images/logo'
 import { usePathname } from 'next/navigation'
 import Credits from '@/components/footer/credits'
+import Company from '@/components/footer/company'
+import NewsLetter from '@/components/footer/newsletter'
+import SocialMedias from '@/components/footer/social-medias'
 
-export default function Footer(props) {
+export default function Footer() {
     const pathname = usePathname()
     const router = useRouter({ version: 'v1', pathname, ...web })
 
     // TODO: Use router web for creating links
     const linkGroups = [
         {
-            title: 'where are we ..!',
-            links: [
-                {
-                    title: 'Oasis Mall, Haridwar Rd, Dehradun, Uttarakhand 248005',
-                    href: '/',
-                },
-            ],
-        },
-        {
-            title: 'Links',
+            title: 'Internal Links',
             links: [
                 {
                     title: 'HOME',
@@ -32,15 +25,36 @@ export default function Footer(props) {
                 },
                 {
                     title: 'ABOUT',
-                    href: '/',
-                },
-                {
-                    title: 'CONTACT',
-                    href: '/',
+                    href: '/about',
                 },
                 {
                     title: 'GALLERY',
+                    href: '/gallery',
+                },
+                {
+                    title: 'CONTACT',
+                    href: '/contact',
+                },
+            ],
+        },
+        {
+            title: 'External Links',
+            links: [
+                {
+                    title: 'HOME',
                     href: '/',
+                },
+                {
+                    title: 'ABOUT',
+                    href: '/about',
+                },
+                {
+                    title: 'GALLERY',
+                    href: '/gallery',
+                },
+                {
+                    title: 'CONTACT',
+                    href: '/contact',
                 },
             ],
         },
@@ -49,87 +63,70 @@ export default function Footer(props) {
             links: [
                 {
                     title: 'Facebook',
-                    href: 'https://www.facebook.com/ausgolguppaywala/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0',
+                    href: 'https://www.facebook.com/ausgolguppaywala/',
                 },
                 {
-                    title: 'Google',
-                    href: '/',
+                    title: 'Instgram',
+                    href: 'https://www.instagram.com/ausgolguppaywala/',
                 },
                 {
                     title: 'Linked in',
                     href: 'https://www.linkedin.com/in/nishant-tomar-3a36534b/',
                 },
                 {
-                    title: 'Instgram',
-                    href: 'https://www.instagram.com/ausgolguppaywala/',
+                    title: 'Google',
+                    href: 'https://www.google.co.in/search?q=australian+golgappe+wala',
                 },
             ],
         },
     ]
 
     return (
-        <footer class="p-4 ">
-            <div class="mx-auto max-w-screen-xl">
-                <div class="md:flex md:justify-between ">
-                    <div class="mb-6 md:mb-0  ">
-                        <NextLink
-                            class="flex items-center"
-                            href={router.getRouteByName('Home')}
-                        >
-                            {/* Company Logo */}
-                            <Logo />
+        <footer className="border-t text-neutral-600 dark:text-neutral-200 lg:text-left">
+            <SocialMedias />
 
-                            {/* Company Title */}
-                            <span class="self-center pl-3 text-2xl font-semibold whitespace-nowrap dark:text-white">
-                                {setting?.shortName}
-                            </span>
-                        </NextLink>
-                    </div>
+            <div className="mx-6 py-10 text-center md:text-left">
+                <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+                    <Company />
 
                     {/* Footer Links */}
-                    <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-                        {linkGroups.map((group, index) => {
-                            const { title, links } = group
+                    {linkGroups.map((group, index) => {
+                        const { title, links } = group
 
-                            if (links.length <= 0) return
+                        if (links.length <= 0) return
 
-                            return (
-                                <div key={`FooterLinkGroup-${index}`}>
-                                    {/* Link Group */}
-                                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                                        {title}
-                                    </h2>
+                        return (
+                            <div className="" key={`FooterLinkGroup-${index}`}>
+                                {/* Link Group */}
+                                <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start text-gray-900 dark:text-white">
+                                    {title}
+                                </h6>
 
-                                    {/* Group Links */}
-                                    <ul class="text-gray-600 dark:text-gray-400">
-                                        {links.map((link, id) => {
-                                            return (
-                                                <li
-                                                    key={`FooterGroupLink-${id}`}
-                                                    class="mb-4"
-                                                >
-                                                    <NextLink
-                                                        href={link?.href}
-                                                        class="hover:underline"
-                                                    >
-                                                        {link.title}
-                                                    </NextLink>
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                            )
-                        })}
-                    </div>
+                                {/* Group Links */}
+                                {links.map((link, id) => {
+                                    return (
+                                        <p
+                                            className="mb-4 flex items-center justify-center md:justify-start"
+                                            key={`FooterGroupLink-${id}`}
+                                        >
+                                            {/* Group Link */}
+                                            <NextLink
+                                                href={link?.href}
+                                                className="text-neutral-800 dark:text-neutral-200 hover:underline"
+                                            >
+                                                {link?.title}
+                                            </NextLink>
+                                        </p>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
                 </div>
-
-                {/* Horizontal Divider */}
-                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-
-                {/* Company Credits */}
-                <Credits />
             </div>
+
+            <NewsLetter />
+            <Credits />
         </footer>
     )
 }
