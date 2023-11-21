1a53ad9b5728be73584cfe8d8c76640d99580879
Raman Verma <devramanverma@gmail.com>
2023-11-21 21:54:41 +0530

    Updated: css class to tailwind class

--- src/app/_components/top/hero-title.js
+++ src/app/_components/top/hero-title.js
@@ -5,7 +5,8 @@ export default function Herotitle() {
         <>
             {/* Hero Title */}
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                Australian <span className="top-span">Gol Guppay</span> Wala
+                Australian <span className="text-[#ffbf31]">Gol Guppay</span>{' '}
+                Wala
             </h1>
         </>
     )
