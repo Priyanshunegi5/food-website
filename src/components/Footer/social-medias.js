cac9cb3d2fb3c99283ba1ceb7815c4226b020a86
Raman Verma <devramanverma@gmail.com>
2023-11-21 23:39:15 +0530

    Added: SocialMedias component

---
+++ src/components/Footer/social-medias.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { map } from 'lodash'
+import { useTheme } from 'next-themes'
+import { socialMedias } from '@/data/social-medias'
+
+export default function SocialMedias() {
+    const { theme } = useTheme()
+
+    return (
+        <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
+            {/* Slogan */}
+            <div className="text-[#ffbf31] mr-12 font-bold hidden lg:block">
+                <span>Get connected with us on social networks:</span>
+            </div>
+
+            {/* Social Medias Icons */}
+            <div className="flex justify-center">
+                {map(socialMedias, (value, key) => {
+                    return (
+                        <a
+                            target="_blank"
+                            href={value.link}
+                            title={value.name}
+                            rel="noopener noreferrer"
+                            key={`CreditSocialMedia-${key}`}
+                            className="mr-6 w-5 h-5 text-neutral-600 dark:text-neutral-200 hover:scale-150"
+                        >
+                            {/* Social Media Icon */}
+                            {React.cloneElement(value?.icon, {
+                                size: 'auto',
+                                color: {
+                                    dark: 'white',
+                                    light: 'black',
+                                }[theme],
+                            })}
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
