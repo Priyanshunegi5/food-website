76208d6a001d64140c5b5a0b7322eec3d77e569c
Raman Verma <devramanverma@gmail.com>
2023-11-21 23:37:05 +0530

    Updated: social medias

--- src/data/social-medias.js
+++ src/data/social-medias.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { FaYoutube, FaFacebook, FaInstagram } from 'react-icons/fa'
+import { FaLinkedin, FaFacebook, FaGoogle, FaInstagram } from 'react-icons/fa'
 
 /**
  * All app social medias will be defined here.
@@ -8,22 +8,27 @@ import { FaYoutube, FaFacebook, FaInstagram } from 'react-icons/fa'
  * Things that are not completly
  * dynamic but still need changes later on
  *
- * @var const SocialMedias
+ * @var const socialMedias
  */
-export const SocialMedias = {
-    youtube: {
-        name: 'Youtube',
-        icon: <FaYoutube />,
-        link: 'https://youtube.com',
-    },
+export const socialMedias = {
     facebook: {
         name: 'Facebook',
         icon: <FaFacebook />,
-        link: 'https://facebook.com',
+        link: 'https://www.facebook.com/ausgolguppaywala/',
     },
     instagram: {
         name: 'Instagram',
         icon: <FaInstagram />,
-        link: 'https://instagram.com',
+        link: 'https://www.instagram.com/ausgolguppaywala/',
+    },
+    linkedin: {
+        name: 'Linkedin',
+        icon: <FaLinkedin />,
+        link: 'https://www.linkedin.com/in/nishant-tomar-3a36534b/',
+    },
+    google: {
+        name: 'Google',
+        icon: <FaGoogle />,
+        link: 'https://www.google.co.in/search?q=australian+golgappe+wala',
     },
 }
