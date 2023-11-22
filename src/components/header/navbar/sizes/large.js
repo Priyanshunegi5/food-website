116f29a04e60c921836ac38f7561bf5d9daa2c3f
Raman Verma <devramanverma@gmail.com>
2023-11-22 13:57:36 +0530

    Added: SquigglyUnderline in navbar link

--- src/components/header/navbar/sizes/large.js
+++ src/components/header/navbar/sizes/large.js
@@ -3,39 +3,56 @@
 import { NavbarItem, NavbarBrand, NavbarContent } from '@nextui-org/react'
 
 import NextLink from 'next/link'
+import { setting } from '@/data/setting'
 import Logo from '@/components/images/logo'
 import ThemeSwitch from '@/components/utils/theme-switch'
+import SquigglyUnderline from '@/components/framer/squiggly-underline'
 
 export default function Large({ router }) {
     return (
         <>
             <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+                {/* Company Description */}
                 <NavbarBrand as="li" className="gap-3 max-w-fit">
                     <NextLink
                         href={router.getRouteByName('Home')}
                         className="flex justify-start items-center gap-1"
                     >
+                        {/* Company Logo */}
                         <Logo />
+
+                        {/* Company Name */}
+                        <p className="font-bold text-inherit">
+                            {setting?.shortName}
+                        </p>
                     </NextLink>
                 </NavbarBrand>
 
                 {/* Navbar Menu */}
                 <ul className="hidden lg:flex gap-4 justify-start ml-2">
-                    {router.getRoutes()?.map((route, index) => (
-                        <NavbarItem
-                            key={`${route.name}-${index}`}
-                            isActive={router.isActiveRoutePath(route.path)}
-                        >
-                            {/* Link to the page */}
-                            <NextLink
-                                href={route.path}
-                                color="foreground"
-                                className="data-[active=true]:text-primary data-[active=true]:font-medium"
+                    {router.getRoutes()?.map((route, index) => {
+                        const isActive = router.isActiveRoutePath(route.path)
+
+                        return (
+                            <NavbarItem
+                                key={`${route.name}-${index}`}
+                                isActive={isActive}
                             >
-                                {route.name}
-                            </NextLink>
-                        </NavbarItem>
-                    ))}
+                                {/* Link to the page */}
+                                <NextLink
+                                    href={route.path}
+                                    color="foreground"
+                                    className="data-[active=true]:text-primary data-[active=true]:font-medium"
+                                >
+                                    {/* Link Name */}
+                                    {route.name}
+
+                                    {/* Underline */}
+                                    {isActive && <SquigglyUnderline />}
+                                </NextLink>
+                            </NavbarItem>
+                        )
+                    })}
                 </ul>
             </NavbarContent>
 
