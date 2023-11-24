fbe95725281c19376599bb6bcd1cc4a8a99f35fd
Raman Verma <devramanverma@gmail.com>
2023-11-25 01:44:16 +0530

    Fixed: height problem

--- src/app/(pages)/contact/_components/google/map.js
+++ src/app/(pages)/contact/_components/google/map.js
@@ -4,10 +4,10 @@ export default function Map() {
             {/* Google Map Iframe Embeded */}
             <iframe
                 width="100%"
-                height="480"
                 loading="lazy"
                 allowfullscreen=""
                 title="google-map"
+                className="h-[80vh]"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
             ></iframe>
         </div>
