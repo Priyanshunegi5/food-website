905287cecbf0d37a2e658241f08d0aed28beacbc
Raman Verma <devramanverma@gmail.com>
2023-11-26 20:54:48 +0530

    Added: preloader customizable opacity

--- src/components/utils/preloader.js
+++ src/components/utils/preloader.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 
 export default function Preloader({
     children,
+    opacity = 90,
     delay = 1000,
     title = null,
     image = null,
@@ -23,7 +24,9 @@ export default function Preloader({
 
             {/* Preloader */}
             {loading && (
-                <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden flex flex-col items-center justify-center bg-default">
+                <div
+                    className={`fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden flex flex-col items-center justify-center bg-default bg-opacity-${opacity} backdrop-blur-lg rounded drop-shadow-lg`}
+                >
                     {/* Preloader Image */}
                     {image && (
                         <Image
