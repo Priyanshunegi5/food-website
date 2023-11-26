c3e952865c01618e8df370df64ec41eea3401277
Raman Verma <devramanverma@gmail.com>
2023-11-26 16:42:13 +0530

    Fixed: text size for responsiveness

--- src/app/_components/top/hero-title.js
+++ src/app/_components/top/hero-title.js
@@ -4,7 +4,7 @@ export default function Herotitle() {
     return (
         <>
             {/* Hero Title */}
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <h1 className="mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-7xl dark:text-white">
                 Australian <span className="text-[#ffbf31]">Gol Guppay</span>{' '}
                 Wala
             </h1>
