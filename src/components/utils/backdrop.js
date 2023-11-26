ef232c02e6d9540eea02cd322d9f6f8c21ce7cc5
Raman Verma <devramanverma@gmail.com>
2023-11-26 23:45:56 +0530

    Applied: backdrop stylesheet

--- src/components/utils/backdrop.js
+++ src/components/utils/backdrop.js
@@ -1,5 +1,7 @@
 'use client'
 
+import '@/components/utils/styles/backdrop.css'
+
 import { useState } from 'react'
 import { Card, CardBody } from '@nextui-org/react'
 import { backdropType } from '@/data/utils/backdrop-type'
