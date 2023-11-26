ed71a2c9a999f7d52127ffe639860cfd3d732d25
Raman Verma <devramanverma@gmail.com>
2023-11-26 21:19:08 +0530

    Updated: background image and video placeholder

--- src/app/_components/hero.js
+++ src/app/_components/hero.js
@@ -9,18 +9,17 @@ export default function Hero() {
     // TODO: Fetch video from the server
     const background = {
         image: {
-            src: 'https://www.udrop.com/file/L97O/pexels-photo-9022271.jpeg',
+            type: 'image/webp',
+            src: 'images/hero/golguppaywala-image.webp',
         },
         video: {
-            src: 'https://www.udrop.com/file/L97L/pexels-tom-leishman-9022271_(Original).mp4',
+            type: 'video/mp4',
+            src: 'images/hero/golguppaywala-video.mp4',
         },
     }
 
     return (
-        <BackgroundVideo
-            imageSource={background?.image?.src}
-            videoSource={background?.video?.src}
-        >
+        <BackgroundVideo image={background?.image} video={background?.video}>
             <section className="lg:pt-10 pt-10">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                     {/* Hero Title */}
