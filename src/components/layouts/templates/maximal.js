f6b89bdc1baaddd33c5d355d48b424d3ecbd6fea
Raman Verma <devramanverma@gmail.com>
2023-11-26 20:48:00 +0530

    Applied: Preloader component

--- src/components/layouts/templates/maximal.js
+++ src/components/layouts/templates/maximal.js
@@ -1,22 +1,33 @@
 import Header from '@/components/header/header'
 import Footer from '@/components/footer/footer'
+import Preloader from '@/components/utils/preloader'
 import Transition from '@/components/transitions/transition'
 
 export default function Maximal(props) {
     return (
         <div className="relative flex flex-col">
-            <div {...props}>
-                {/* Header Section */}
-                <Header />
+            <Preloader
+                delay="1500"
+                image={{
+                    width: 100,
+                    height: 100,
+                    alt: 'panipuri',
+                    src: '/images/preloaders/panipuri.webp',
+                }}
+            >
+                <div {...props}>
+                    {/* Header Section */}
+                    <Header />
 
-                {/* Content Section */}
-                <main>
-                    <Transition>{props.children}</Transition>
-                </main>
+                    {/* Content Section */}
+                    <main>
+                        <Transition>{props.children}</Transition>
+                    </main>
 
-                {/* Footer Section */}
-                <Footer />
-            </div>
+                    {/* Footer Section */}
+                    <Footer />
+                </div>
+            </Preloader>
         </div>
     )
 }
