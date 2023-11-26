26d7ef9db729ccb0786c8163c3673aa40c958473
Raman Verma <devramanverma@gmail.com>
2023-11-26 20:53:35 +0530

    Refactored: for using page as backdrop

--- src/components/utils/preloader.js
+++ src/components/utils/preloader.js
@@ -16,42 +16,44 @@ export default function Preloader({
         setTimeout(() => setLoading(false), delay)
     }, [])
 
-    if (!loading) {
-        return children
-    }
-
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden flex flex-col items-center justify-center">
-                {/* Preloader Image */}
-                {image && (
-                    <Image
-                        src={image?.src}
-                        width={image?.width}
-                        height={image?.height}
-                        alt={image?.alt ?? image?.src}
-                        className="ease-linear rounded-full h-[10vh] w-[10vh] mb-4"
-                    />
-                )}
+            {/* Underlying Page */}
+            {children}
 
-                {(title || description) && (
-                    <>
-                        {/* Preloader Title */}
-                        {title && (
-                            <h2 className="text-center text-white text-xl font-semibold">
-                                {title}
-                            </h2>
-                        )}
+            {/* Preloader */}
+            {loading && (
+                <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden flex flex-col items-center justify-center bg-default">
+                    {/* Preloader Image */}
+                    {image && (
+                        <Image
+                            src={image?.src}
+                            width={image?.width}
+                            height={image?.height}
+                            alt={image?.alt ?? image?.src}
+                            className="shadow ease-linear rounded-full h-[10vh] w-[10vh] mb-4"
+                        />
+                    )}
 
-                        {/* Preloader Description */}
-                        {description && (
-                            <p className="w-1/3 text-center text-white">
-                                {description}
-                            </p>
-                        )}
-                    </>
-                )}
-            </div>
+                    {(title || description) && (
+                        <>
+                            {/* Preloader Title */}
+                            {title && (
+                                <h2 className="text-center text-white text-xl font-semibold">
+                                    {title}
+                                </h2>
+                            )}
+
+                            {/* Preloader Description */}
+                            {description && (
+                                <p className="w-1/3 text-center text-white">
+                                    {description}
+                                </p>
+                            )}
+                        </>
+                    )}
+                </div>
+            )}
         </>
     )
 }
