ad03ba4c19bf6d425b0e02e7951ea1fc0071258f
Raman Verma <devramanverma@gmail.com>
2023-11-27 15:35:46 +0530

    Fixed: backdrop image not loading

--- src/app/(pages)/blog/[slug]/_components/top/backdrop-poster.js
+++ src/app/(pages)/blog/[slug]/_components/top/backdrop-poster.js
@@ -4,10 +4,9 @@ export default function BackdropPoster({ poster }) {
             {/* Article Poster Backdrop */}
             <div
                 style={{
-                    height: '75vh;',
                     backgroundImage: `url(${poster})`,
                 }}
-                className="container w-full max-w-6xl mx-auto bg-cover mt-8 bg-zinc-300 dark:bg-zinc-800 rounded"
+                className="container w-full h-[75vh] max-w-6xl mx-auto bg-cover mt-8 bg-zinc-300 dark:bg-zinc-800 rounded"
             ></div>
         </>
     )
