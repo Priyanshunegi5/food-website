ffb1c7e4004b4defd3d2daad4e9ac6aa72e5d969
Raman Verma <devramanverma@gmail.com>
2023-11-28 13:59:27 +0530

    Moved: From comming-soon\comming-soon.js to comming-soon.js

---
+++ src/app/_components/comming-soon.js
@@ -0,0 +1,21 @@
+export default function CommingSoon() {
+    return (
+        <section className="">
+            <div class="py-5 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
+                {/* Menu Title */}
+                <h1 class="drop-shadow text-4xl font-bold tracking-tight leading-none text-gray-900 mb-4 lg:mb-6 dark:text-white">
+                    Comming{' '}
+                    <span className="text-naples-yellow-300 dark:text-naples-yellow-600">
+                        {' '}
+                        Soon{' '}
+                    </span>
+                </h1>
+
+                {/* Menu description */}
+                <p class="font-light text-gray-500 md:text-lg xl:text-xl dark:text-gray-400">
+                    Our New Branch In Sydney
+                </p>
+            </div>
+        </section>
+    )
+}
