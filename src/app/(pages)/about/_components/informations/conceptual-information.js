90971052840015e96b9d7e7b9fcd1b691957eb85
priyanshunegi5 <priyanshunegi3668@gmail.com>
2023-11-29 17:13:51 +0530

    update the about us section datan...!

--- src/app/(pages)/about/_components/informations/conceptual-information.js
+++ src/app/(pages)/about/_components/informations/conceptual-information.js
@@ -11,16 +11,14 @@ export default function ConceptualInformation() {
                             alt="dashboard image"
                             className="w-full dark:hidden"
                             src={
-                                'https://source.unsplash.com/random/990x715?sig=' +
-                                Date.now()
+                                'https://australiangolguppaywala.com/images/fastfood/about.jpg'
                             }
                         />
                         <Image
                             alt="dashboard image"
                             className="w-full hidden dark:block"
                             src={
-                                'https://source.unsplash.com/random/990x715?sig=' +
-                                Date.now()
+                                'https://australiangolguppaywala.com/images/fastfood/about.jpg'
                             }
                         />
                     </div>
@@ -29,24 +27,21 @@ export default function ConceptualInformation() {
                     <div className="mt-4 md:mt-0">
                         {/* Title */}
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                            Why do we use it?
+                            Best gol guppay wala in Dehradun
                         </h2>
 
                         {/* Description */}
                         <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400 text-justify">
-                            It is a long established fact that a reader will be
-                            distracted by the readable content of a page when
-                            looking at its layout. The point of using Lorem
-                            Ipsum is that it has a more-or-less normal
-                            distribution of letters, as opposed to using
-                            'Content here, content here', making it look like
-                            readable English. Many desktop publishing packages
-                            and web page editors now use Lorem Ipsum as their
-                            default model text, and a search for 'lorem ipsum'
-                            will uncover many web sites still in their infancy.
-                            Various versions have evolved over the years,
-                            sometimes by accident, sometimes on purpose
-                            (injected humour and the like).
+                            Located in Jogiwala, this eatery, known as
+                            Australian Gol Guppy Wala, serves golgappas with
+                            diverse fillings and flavors, providing an
+                            assortment of tastes to its customers Dehradun might
+                            have a variety of popular places where you can enjoy
+                            golgappa, The street food of India is probably the
+                            best in the world. You can fall in a love affair
+                            with the lip-smacking food .I am a very big fan of
+                            street food and I have tasted street food from all
+                            parts of India.The street foods that I love are ...!
                         </p>
                     </div>
                 </div>
