04bd6bdc8ff113d7c55b1edae4e2a0ca8c258560
Raman Verma <devramanverma@gmail.com>
2023-11-30 00:39:25 +0530

    Refactoring: rewrite code and updated content

--- src/app/(pages)/about/_components/hero.js
+++ src/app/(pages)/about/_components/hero.js
@@ -2,34 +2,43 @@ import { Image } from '@nextui-org/react'
 
 export default function Hero() {
     return (
-        <section className="">
-            <div className="flex relative z-20 items-center overflow-hidden">
-                <div className="container mx-auto px-6 flex relative py-16">
-                    <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-                        <span className="w-20 h-2  mb-12"> </span>
-
+        <section>
+            <div class="py-20">
+                <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+                    <div class="flex flex-col w-full lg:w-1/2 justify-center p-8">
                         {/* Title */}
-                        <h1 className="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
-                            About Us
-                            {/* Icon */}
-                            <span className="text-5xl sm:text-7xl">💓</span>
+                        <h1 class="uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800 mb-2">
+                            About
                         </h1>
 
-                        {/* Description */}
-                        <p className="text-sm pt-5 sm:text-base text-gray-700 dark:text-white text-justify">
-                            After their successful stint in Australia,
-                            experiencing different cultures and cuisines, the
-                            two friends, Honey & Nishant, decided it was time to
-                            move back to their roots in India to start a new
-                            phase of their lives with "Australian Gol Guppay
-                            Wala".
+                        {/* Icon */}
+                        <h2 class="text-3xl md:text-4xl leading-relaxed md:leading-snug mb-2">
+                            {/* <span className="text-5xl sm:text-7xl">💓</span> */}
+                            <span className="uppercase">{' us '}</span>
+                            <span className="text-poppy-500 dark:text-sandy-brown-500 text-4xl sm:text-5xl">
+                                AusGGW
+                            </span>
+                        </h2>
+
+                        <p class="text-sm pt-5 sm:text-base text-gray-700 dark:text-white text-justify">
+                            Honey and Nishant, after a successful stint in
+                            Australia, returned to India to start “Australian
+                            Gol Guppay Wala”. They were inspired by the vibrant
+                            street food scene in India, particularly the chaat
+                            stalls. They traveled across India, learning about
+                            the secret spices, perfect chutneys, and the art of
+                            balancing flavors. Finally, they opened “Australian
+                            Gol Guppay Wala” in Dehradun, offering a wide
+                            variety of chaat options from different regions of
+                            India.
                         </p>
                     </div>
 
-                    {/* Image */}
-                    <div className="hidden sm:block sm:w-1/3 lg:w-3/5  relative">
-                        <div className="max-w-xs md:max-w-sm m-auto">
-                            <Image src="/images/company/nishant.jpg" />
+                    <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3">
+                        <div class="h-48 flex flex-wrap content-center">
+                            <div>
+                                <Image src="https://www.udrop.com/file/L98m/Nishant.jpg" />
+                            </div>
                         </div>
                     </div>
                 </div>
