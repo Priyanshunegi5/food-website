5539feb5035b235ca2346ca16509388e0d5ec758
Raman Verma <devramanverma@gmail.com>
2023-11-30 02:39:36 +0530

    Removed: WhyUs component

--- src/app/page.js
+++ src/app/page.js
@@ -48,9 +48,6 @@ export default function Home() {
 
             {/* References */}
             <>
-                {/* Why Us */}
-                <WhyUs />
-
                 {/* From People */}
                 <Reviews />
 
