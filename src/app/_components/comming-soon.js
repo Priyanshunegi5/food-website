02ec09f37ea760983cb28bac70f1f6564e731d01
Raman Verma <devramanverma@gmail.com>
2023-11-30 02:25:27 +0530

    Applied: animation effect

--- src/app/_components/comming-soon.js
+++ src/app/_components/comming-soon.js
@@ -3,7 +3,7 @@ export default function CommingSoon() {
         <section className="">
             <div class="py-5 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
                 {/* Menu Title */}
-                <h1 class="drop-shadow text-4xl font-bold tracking-tight leading-none text-gray-900 mb-4 lg:mb-6 dark:text-white">
+                <h1 class="animate-pulse drop-shadow text-4xl font-bold tracking-tight leading-none text-gray-900 mb-4 lg:mb-6 dark:text-white">
                     Comming{' '}
                     <span className="text-naples-yellow-300 dark:text-naples-yellow-600">
                         {' '}
