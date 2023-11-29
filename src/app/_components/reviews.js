455be4d3aba941d4f62589dcd7435365ce49aedf
Raman Verma <devramanverma@gmail.com>
2023-11-30 02:26:33 +0530

    Refactored: rewrited review section code

--- src/app/_components/reviews.js
+++ src/app/_components/reviews.js
@@ -1,5 +1,6 @@
-import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
+import { map, chunk } from 'lodash'
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
+import { Card, CardBody, CardHeader, Image } from '@nextui-org/react'
 
 export default function Reviews() {
     // TODO: Fetch reviews from the server
@@ -103,73 +104,175 @@ export default function Reviews() {
     ]
 
     return (
-        <section className="bg-stone-200 dark:bg-stone-950 m-10 rounded-2xl">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
-                <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
-                    {/* Title */}
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                        Our Reviews
-                    </h2>
+        // <section className="bg-stone-200 dark:bg-stone-950 m-10 rounded-2xl">
+        //     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+        //         <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+        //             {/* Title */}
+        //             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        //                 Our Reviews
+        //             </h2>
 
-                    {/* Description */}
-                    <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-                        here are some review
-                    </p>
+        //             {/* Description */}
+        //             <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+        //                 here are some review
+        //             </p>
+        //         </div>
+
+        //         <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        //             {reviews.map((review, index) => {
+        //                 return (
+        //                     <div
+        //                         key={`Review-${index}`}
+        //                         className="my-10 bg-stone-300 hover:bg-stone-400 dark:bg-stone-700 hover:dark:bg-stone-900 rounded-lg hover:scale-105"
+        //                     >
+        //                         {/* Review Header */}
+        //                         <div className="flex justify-center">
+        //                             {/* Author Image */}
+        //                             <div className="flex justify-center -mt-[75px]">
+        //                                 <Image
+        //                                     shadow="md"
+        //                                     radius="full"
+        //                                     alt={review?.author}
+        //                                     src={review?.image?.src}
+        //                                     className="border-3 border-slate-300/50 dark:border-default-100/50 p-0 hover:p-1 w-32 h-32 rounded-full shadow-lg dark:shadow-black/30 hover:scale-110"
+        //                                 />
+        //                             </div>
+        //                         </div>
+
+        //                         {/* Review Body */}
+        //                         <div className="block h-full">
+        //                             <div className="p-6">
+        //                                 {/* Author Name */}
+        //                                 <h5 className="mb-3 text-lg font-bold">
+        //                                     {review?.author}
+        //                                 </h5>
+
+        //                                 {/* Author Position */}
+        //                                 <h6 className="mb-1 text-sm">
+        //                                     {review?.position}
+        //                                 </h6>
+
+        //                                 <p className="mb-6">
+        //                                     {/* Quote Left Icon */}
+        //                                     <FaQuoteLeft
+        //                                         size="10"
+        //                                         className="inline-block"
+        //                                     />
+
+        //                                     {/* Author Review */}
+        //                                     <span> {review?.message} </span>
+
+        //                                     {/* Quote Right Icon*/}
+        //                                     <FaQuoteRight
+        //                                         size="10"
+        //                                         className="inline-block"
+        //                                     />
+        //                                 </p>
+        //                             </div>
+        //                         </div>
+        //                     </div>
+        //                 )
+        //             })}
+        //         </div>
+        //     </div>
+        // </section>
+
+        <section id="testimonies" class="bg-zinc-200 dark:bg-zinc-950 py-20">
+            <div class="max-w-6xl mx-8 md:mx-10 lg:mx-20 xl:mx-auto">
+                <div class="transition duration-500 ease-in-out transform scale-100 translate-x-0 translate-y-0 opacity-100">
+                    <div class="mb-12 space-y-5 md:mb-16 md:text-center">
+                        {/* Title */}
+                        <div class="text-gray-900 dark:text-white inline-block px-3 py-1 text-sm font-semibold rounded-lg md:text-center text-cn hover:cursor-pointer hover:bg-opacity-40">
+                            Our Reviews
+                        </div>
+
+                        {/* Subtitle */}
+                        <h1 class="text-gray-900 dark:text-white mb-5 text-3xl font-semibol md:text-center md:text-5xl">
+                            It's not just us.
+                        </h1>
+
+                        {/* Description */}
+                        <p class="text-gray-900 dark:text-white text-xl md:text-center md:text-2xl">
+                            Here's what others have to say about us.
+                        </p>
+                    </div>
                 </div>
 
-                <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                    {reviews.map((review, index) => {
+                <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+                    {map(chunk(reviews, 4), (chunks, id) => {
                         return (
-                            <div
-                                key={`Review-${index}`}
-                                className="my-10 bg-stone-300 hover:bg-stone-400 dark:bg-stone-700 hover:dark:bg-stone-900 rounded-lg hover:scale-105"
-                            >
-                                {/* Review Header */}
-                                <div className="flex justify-center">
-                                    {/* Author Image */}
-                                    <div className="flex justify-center -mt-[75px]">
-                                        <Image
-                                            shadow="md"
-                                            radius="full"
-                                            alt={review?.author}
-                                            src={review?.image?.src}
-                                            className="border-3 border-slate-300/50 dark:border-default-100/50 p-0 hover:p-1 w-32 h-32 rounded-full shadow-lg dark:shadow-black/30 hover:scale-110"
-                                        />
-                                    </div>
-                                </div>
+                            <ul class="space-y-8" key={`ReviewChunk-${id}`}>
+                                {map(chunks, (review, index) => {
+                                    return (
+                                        <li
+                                            class="text-sm leading-6"
+                                            key={`Review-${index}`}
+                                        >
+                                            <div class="relative group">
+                                                <div class="absolute transition rounded-lg opacity-25 -inset-1 bg-gradient-to-r from-poppy-600 to-xanthous-600 blur duration-400 group-hover:opacity-100 group-hover:duration-200"></div>
+                                                <div class="cursor-pointer">
+                                                    <div class="relative p-6 space-y-6 leading-none rounded-lg bg-white dark:bg-jet-500 ring-1 ring-gray-900/5">
+                                                        <div class="flex items-center space-x-4">
+                                                            {/* Author Avatar */}
+                                                            <Image
+                                                                shadow="md"
+                                                                radius="full"
+                                                                alt={
+                                                                    review?.author
+                                                                }
+                                                                src={
+                                                                    review
+                                                                        ?.image
+                                                                        ?.src
+                                                                }
+                                                                className="w-12 h-12 bg-center bg-cover border rounded-full"
+                                                            />
 
-                                {/* Review Body */}
-                                <div className="block h-full">
-                                    <div className="p-6">
-                                        {/* Author Name */}
-                                        <h5 className="mb-3 text-lg font-bold">
-                                            {review?.author}
-                                        </h5>
+                                                            <div>
+                                                                {/* Author Name */}
+                                                                <h3 class="text-lg font-semibold">
+                                                                    {
+                                                                        review?.author
+                                                                    }
+                                                                </h3>
 
-                                        {/* Author Position */}
-                                        <h6 className="mb-1 text-sm">
-                                            {review?.position}
-                                        </h6>
+                                                                {/* Author Position */}
+                                                                <p class="text-md">
+                                                                    {
+                                                                        review?.position
+                                                                    }
+                                                                </p>
+                                                            </div>
+                                                        </div>
 
-                                        <p className="mb-6">
-                                            {/* Quote Left Icon */}
-                                            <FaQuoteLeft
-                                                size="10"
-                                                className="inline-block"
-                                            />
+                                                        <p class="leading-normal text-md">
+                                                            {/* Quote Left Icon */}
+                                                            <FaQuoteLeft
+                                                                size="10"
+                                                                className="inline-block"
+                                                            />
 
-                                            {/* Author Review */}
-                                            <span> {review?.message} </span>
+                                                            {/* Author Review */}
+                                                            <span>
+                                                                {' '}
+                                                                {
+                                                                    review?.message
+                                                                }{' '}
+                                                            </span>
 
-                                            {/* Quote Right Icon*/}
-                                            <FaQuoteRight
-                                                size="10"
-                                                className="inline-block"
-                                            />
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
+                                                            {/* Quote Right Icon*/}
+                                                            <FaQuoteRight
+                                                                size="10"
+                                                                className="inline-block"
+                                                            />
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
                         )
                     })}
                 </div>
