1ce2d1fd9f911ee5845477e4d355bd4f62707000
Raman Verma <devramanverma@gmail.com>
2023-11-30 00:46:28 +0530

    Updated: contents

--- src/app/(pages)/about/_components/informations/conceptual-information.js
+++ src/app/(pages)/about/_components/informations/conceptual-information.js
@@ -27,21 +27,20 @@ export default function ConceptualInformation() {
                     <div className="mt-4 md:mt-0">
                         {/* Title */}
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                            Best gol guppay wala in Dehradun
+                            From Australia to India: The Birth of a Culinary
+                            Venture
                         </h2>
 
                         {/* Description */}
                         <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400 text-justify">
-                            Located in Jogiwala, this eatery, known as
-                            Australian Gol Guppy Wala, serves golgappas with
-                            diverse fillings and flavors, providing an
-                            assortment of tastes to its customers Dehradun might
-                            have a variety of popular places where you can enjoy
-                            golgappa, The street food of India is probably the
-                            best in the world. You can fall in a love affair
-                            with the lip-smacking food .I am a very big fan of
-                            street food and I have tasted street food from all
-                            parts of India.The street foods that I love are ...!
+                            Their journey began in Australia, where they were
+                            exposed to a variety of cultures and cuisines. This
+                            experience broadened their culinary horizons and
+                            sparked a desire to bring a piece of their homeland
+                            to the global stage. Their love for food and their
+                            roots led them back to India, where they decided to
+                            embark on a new venture, “Australian Gol Guppay
+                            Wala”.
                         </p>
                     </div>
                 </div>
