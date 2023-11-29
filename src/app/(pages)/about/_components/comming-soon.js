2b753630af187f95f0fdf5b4231f04be7c910d28
Raman Verma <devramanverma@gmail.com>
2023-11-30 01:25:08 +0530

    Added: CommingSoon component

---
+++ src/app/(pages)/about/_components/comming-soon.js
@@ -0,0 +1,21 @@
+export default function CommingSoon() {
+    return (
+        <section className="">
+            <div class="py-5 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
+                {/* Menu Title */}
+                <h1 class="drop-shadow text-4xl font-bold tracking-tight leading-none text-gray-900 mb-4 lg:mb-6 dark:text-white">
+                    Comming{' '}
+                    <span className="text-naples-yellow-300 dark:text-naples-yellow-600">
+                        {' '}
+                        Soon{' '}
+                    </span>
+                </h1>
+
+                {/* Menu description */}
+                <p class="uppercase text-5xl sm:text-7xl font-black flex flex-col leading-none dark:text-white text-gray-800 mb-2">
+                    App
+                </p>
+            </div>
+        </section>
+    )
+}
