70cdfb6d5ee09beae398fe715acc3ec56391f106
Raman Verma <devramanverma@gmail.com>
2023-11-30 03:41:39 +0530

    Implemented: react-accessible-accordion in faqs

--- src/app/(pages)/contact/_components/faqs.js
+++ src/app/(pages)/contact/_components/faqs.js
@@ -1,4 +1,15 @@
+'use client'
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemPanel,
+    AccordionItemButton,
+    AccordionItemHeading,
+} from 'react-accessible-accordion'
+
 import FaqIcon from '@/components/icons/faq-icon'
+import ArrowUpIcon from '@/components/icons/arrow-up-icon'
 
 export default function Faqs() {
     // TODO: Fetch faqs form the server
@@ -58,36 +69,8 @@ export default function Faqs() {
                     />
                 ),
             },
-            {
-                title: 'What are the key ingredients for making Golgappa?',
-                description: `
-                The main components for Golgappa include semolina or wheat flour for puris, boiled potatoes, chickpeas or sprouts for filling, and a blend of mint, coriander, tamarind, and spices for the pani..
-                `,
-                icon: (
-                    <FaqIcon
-                        className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
-                        filled
-                    />
-                ),
-            },
         ],
         [
-            {
-                title: 'What is Lorem Ipsum?',
-                description: `
-                    You will have access to download the full Figma
-                    project including all of the pages, the
-                    components, responsive pages, and also the
-                    icons, illustrations, and images included in the
-                    screens.
-                `,
-                icon: (
-                    <FaqIcon
-                        className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
-                        filled
-                    />
-                ),
-            },
             {
                 title: 'Can I make Golgappa at home?',
                 description: `
@@ -101,13 +84,9 @@ export default function Faqs() {
                 ),
             },
             {
-                title: 'What is Lorem Ipsum?',
+                title: 'What are the key ingredients for making Golgappa?',
                 description: `
-                    You will have access to download the full Figma
-                    project including all of the pages, the
-                    components, responsive pages, and also the
-                    icons, illustrations, and images included in the
-                    screens.
+                The main components for Golgappa include semolina or wheat flour for puris, boiled potatoes, chickpeas or sprouts for filling, and a blend of mint, coriander, tamarind, and spices for the pani..
                 `,
                 icon: (
                     <FaqIcon
@@ -152,34 +131,48 @@ export default function Faqs() {
                 </h2>
 
                 {/* (Faqs) Frequently Asked Questions */}
-                <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+                <Accordion
+                    allowZeroExpanded="true"
+                    allowMultipleExpanded="true"
+                    className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2"
+                >
                     {faqs.map((chunk, index) => {
                         return (
                             <div key={`FaqChunk-${index}`}>
                                 {chunk.map((faq, id) => {
                                     return (
-                                        <div
-                                            className="mb-10"
-                                            key={`FaqChunk-${index}:${id}`}
+                                        <AccordionItem
+                                            uuid={`FaqChunk-${index}:${id}`}
                                         >
-                                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                                                {/* Icon */}
-                                                {faq?.icon}
-                                                {/* Title */}
-                                                <span>{faq?.title}</span>
-                                            </h3>
+                                            <AccordionItemHeading>
+                                                <AccordionItemButton className="flex items-center justify-between w-full py-5 text-lg font-medium text-left text-gray-900 dark:text-white transition-all ease-in cursor-pointer">
+                                                    <div className="flex flex-row">
+                                                        {/* Icon */}
+                                                        {faq?.icon}
 
-                                            {/* Description */}
-                                            <p className="text-gray-500 dark:text-gray-400 px-5">
-                                                {faq?.description}
-                                            </p>
-                                        </div>
+                                                        {/* Title */}
+                                                        <span>
+                                                            {faq?.title}
+                                                        </span>
+                                                    </div>
+
+                                                    {/* Arrow Up Icon */}
+                                                    <ArrowUpIcon filled />
+                                                </AccordionItemButton>
+                                            </AccordionItemHeading>
+                                            <AccordionItemPanel className="py-5 transition-all duration-300 ease-in-out">
+                                                {/* Description */}
+                                                <p className="mb-2 text-gray-500 dark:text-gray-400 text-justify">
+                                                    {faq?.description}
+                                                </p>
+                                            </AccordionItemPanel>
+                                        </AccordionItem>
                                     )
                                 })}
                             </div>
                         )
                     })}
-                </div>
+                </Accordion>
             </div>
         </section>
     )
